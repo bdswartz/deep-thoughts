d3cc798a8e0d3fb7d7b1603061cd0b9a3ac06057
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-05-15 09:45:39 -0400

    back end querries and mutations complete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 // establish PORT variable 
 const PORT = process.env.PORT || 3001;
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => req.headers
+  context: authMiddleware
 });
 
 // Create express app variable
