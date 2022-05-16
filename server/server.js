608f4e7a474428bbf041f9d92c8997ee17b569e3
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-05-15 22:54:49 -0400

    React server and concurently scripts complete

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 // import express and Apollo
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
@@ -23,6 +24,16 @@ const app = express();
 // establish parsing to handle gettig info to right format
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
