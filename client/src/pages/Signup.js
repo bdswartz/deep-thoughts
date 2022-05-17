1fb2b34c21255583c8af92682d3a99d4bfd95dd6
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-05-17 08:07:17 -0400

    put in auth using JWT and got the login and signup features working

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  // set up the useMutation Hook to give us a closure that returns the addUser f(x) and any errors
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,10 +18,22 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-  };
+  // submit form (notice the async!)
+const handleFormSubmit = async event => {
+  event.preventDefault();
+
+  // use try/catch instead of promises to handle errors
+  try {
+    // execute addUser mutation and pass in variable data from form
+    const { data } = await addUser({
+      // use the formState variables to send to addUser mutation function
+      variables: { ...formState }
+    });
+    Auth.login(data.addUser.token);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -56,6 +73,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
